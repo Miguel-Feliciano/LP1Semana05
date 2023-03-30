a91ab3bdb2ed71551d42775ec43d0e3e4a88189e
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-30 21:18:03 +0100

    Completed ecercise 4

---
+++ Tempo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Tempo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            crono1.Start();
+
+            crono1.Start();
+
+            Thread.Sleep(500);
+
+            crono2.Start();
+
+            Thread.Sleep(300);
+
+            crono1.Stop();
+
+            crono2.Stop();
+
+            Console.WriteLine($"Crono1 time: {crono1.ElapsedMilliseconds / 1000.0:F2} seconds");
+            Console.WriteLine($"Crono2 time: {crono2.ElapsedMilliseconds / 1000.0:F2} seconds");
+        }
+    }
+}
