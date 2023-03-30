a5517a8f958c54a4d099950c6598a66076520f1c
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-30 21:53:58 +0100

    completed exercise 7

---
+++ RandomDice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RandomDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of dice: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int dice = random.Next(1, 7);
+                Console.WriteLine("Dice {0}: {1}", i + 1, dice);
+                sum += dice;
+            }
+
+            Console.WriteLine("Total sum: {0}", sum);
+        }
+    }
+}
+
