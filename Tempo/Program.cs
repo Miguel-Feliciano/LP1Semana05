c03b9ce6525a49fed85e5201cb2bceeaf5d2daea
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-30 21:36:04 +0100

    added coments in response to exercise 5

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -6,9 +6,15 @@ namespace Tempo
 {
     class Program
     {
-
+        //For this program we used the Stopwatch class
         static void Main(string[] args)
         {
+            /// <summary>
+            /// These are the instances created from the Stopwatch class
+            /// They have different states like IsRunning or ElapsedMilliseconds
+            /// You change their state by calling one of its methods
+            /// </summary>
+            /// <returns>They return a stopwatch</returns>
             Stopwatch crono1 = new Stopwatch();
             Stopwatch crono2 = new Stopwatch();
 
