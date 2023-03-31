32402b39bb561fb93b41c45396ac41845a732488
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-31 10:30:28 +0100

    converted strings to upper in exercise 9

---
+++ UpperUnion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UpperUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string united;
+            for (int i = 0; i < args.Length; i++)
+            {
+                args[i] = args[i].ToUpper();
+            }
+        }
+    }
+}
